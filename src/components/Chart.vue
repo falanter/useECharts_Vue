09db361e82b2fc97f200d3f8b3a8ec44a59d67d5
falanter <1654551076@qq.com>
2022-04-19 12:55:07 +0800

    pack code

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,85 +1,102 @@
 <template>
-    <div class="Echarts">
-        <button @click="change">change</button>
-        <div id="main" style="width:600px;height:400px"></div>
-        <div id="bing" style="width:600px;height:400px"></div>
+  <div class="Echarts">
+    <div>
+      <div id="main" style="width: 600px; height: 400px"></div>
+      <div id="bing" style="width: 600px; height: 400px"></div>
+      <div id="line" style="width: 600px; height: 400px"></div>
     </div>
+    <div class="down">
+      <button @click="change">change</button>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-   name:'Echarts',
-   methods:{
-       change(){
-           let myChart2=this.$echarts.init(document.getElementById('bing'));
-            let optionBing={
-                series:[
-                    {
-                        type:'pie',
-                        data:[
-                            {
-                                value:200,
-                                name:'直接访问'
-                            },{
-                                value:335,
-                                name:'广告'
-                            },{
-                                value:100,
-                                name:'搜索'
-                            }
-                        ]
-                    }
-                ]
-            }
-            myChart2.setOption(optionBing)
-       },
-       myEcharts(){
-           let myChart=this.$echarts.init(document.getElementById('main'));
-           let option={
-               title:{
-                   text:'ECharts 入门示例'
-               },
-               tooltip:{},
-               legend:{
-                   data:['销量']
-               },
-               xAxis:{
-                   data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-               },
-               yAxis:{},
-               series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-			  }]
-           }
-            
-            let myChart2=this.$echarts.init(document.getElementById('bing'));
-            let optionBing={
-                series:[
-                    {
-                        type:'pie',
-                        data:[
-                            {
-                                value:100,
-                                name:'直接访问'
-                            },{
-                                value:200,
-                                name:'广告'
-                            },{
-                                value:335,
-                                name:'搜索'
-                            }
-                        ]
-                    }
-                ]
-            }
-            // 使用刚指定的配置项和数据显示图表。
-		    myChart.setOption(option);
-            myChart2.setOption(optionBing)
-        }
+  name: "Echarts",
+  data() {
+    return {
+        barData:[5, 20, 36, 10, 10, 20],
+        barData_new:[],
+        optionBing:{
+        series: [
+          {
+            type: "pie",
+            data: [],
+          },
+        ],
+      },
+      bingData: [{value: 100,name: "直接访问",},{value: 200,name: "广告",},{value: 335,name: "搜索",},],
+      bingData_new: [{value: 335,name: "直接访问",},{value: 200,name: "广告",},{value: 100,name: "搜索",},],
+    };
+  },
+  methods: {
+    change() {
+      let myChart2 = this.$echarts.init(document.getElementById("bing"));
+      this.optionBing.series[0].data=this.bingData_new
+      myChart2.setOption(this.optionBing);
     },
-    mounted(){
-        this.myEcharts();
-    }
-}
+    myEcharts() {
+      let myChart = this.$echarts.init(document.getElementById("main"));
+      let option = {
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"],
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: this.barData,
+          },
+        ],
+      };
+
+      let myChart2 = this.$echarts.init(document.getElementById("bing"));
+      this.optionBing.series[0].data=this.bingData
+
+      let myChart3 = this.$echarts.init(document.getElementById("line"));
+      let optionLine = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { type: 'cross' }
+        },
+        xAxis: {
+            data: ['A', 'B', 'C', 'D', 'E']
+        },
+        yAxis: {},
+        series: [
+          {
+            type: "line",
+            data: [10, 22, 28, 23, 19],
+            smooth:true
+          },
+        ],
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+      myChart2.setOption(this.optionBing);
+      myChart3.setOption(optionLine);
+    },
+  },
+  mounted() {
+    this.myEcharts();
+  },
+};
 </script>
+<style>
+.Echarts {
+  display: flex;
+  flex-direction: column;
+}
+.Echarts div {
+  display: inherit;
+}
+</style>
